341ae810327edb72dbb9d99cf95af44db6221372
domzki156-wq <domzki156@gmail.com>
2025-11-08 08:29:08 +0800

    Update UserDao.java

--- Trackify/src/main/java/App/dao/UserDao.java
+++ Trackify/src/main/java/App/dao/UserDao.java
@@ -1,11 +1,13 @@
 package App.dao;
 
 import App.models.User;
+
 import java.util.Optional;
 
 public interface UserDao {
-    User save(User user);
-    Optional<User> findByUsername(String username);
     Optional<User> findById(String id);
-    void deleteAll(); // for tests/dev
+    Optional<User> findByUsername(String username);
+    User save(User u);
+    void updateBalance(String userId, double newBalance);
+    void deleteById(String id);
 }
